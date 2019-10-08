59b5c0dbde5385879b2efef3df59e5501f5ffc1a
Nenya <nenyalote@yandex.ru>
2019-10-08 21:44:06 +0300

    Решила задачи

--- src/main/java/lesson01/part1/Task01.java
+++ src/main/java/lesson01/part1/Task01.java
@@ -15,6 +15,7 @@ public class Task01 {
      */
 
     public static void main(String[] args) {
-        // Напишите здесь ваш код
+        // вызов метода ...
+        System.out.println("Hello World!");
     }
 }
